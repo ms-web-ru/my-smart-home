416dfd94a327c2ba1427fe857f5560a102e87bfa
ms-web-ru <webmaster@ms-web.ru>
2021-05-13 22:08:51 +0500

    added copyrights

--- app/src/main/java/com/example/mysmarthome3/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/mysmarthome3/ui/home/HomeFragment.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2021. Boost Software License 1.0
+ * @author Mikhail Sayapin dev@ms-web.ru
+ */
+
 package com.example.mysmarthome3.ui.home;
 
 import android.annotation.SuppressLint;
